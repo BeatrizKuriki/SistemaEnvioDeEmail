a058f663fd351c40a52a3906c90ebbc52383f728
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-10-05 01:08:45 -0300

    feat: EmailResource and EmailService

--- integrations/src/main/java/com/kurikibeatriz/integrations/resources/EmailResource.java
+++ integrations/src/main/java/com/kurikibeatriz/integrations/resources/EmailResource.java
@@ -1,5 +1,6 @@
 package com.kurikibeatriz.integrations.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,13 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kurikibeatriz.integrations.dto.EmailDTO;
+import com.kurikibeatriz.integrations.services.EmailService;
 
 @RestController
 @RequestMapping(value = "/emails")
 public class EmailResource {
 	
+	@Autowired
+	private EmailService service;
+	
+	
 	@PostMapping
 	public ResponseEntity<Void> send(@RequestBody EmailDTO dto){
+		service.sendEmail(dto);
 		return ResponseEntity.noContent().build();
 		
 	}
